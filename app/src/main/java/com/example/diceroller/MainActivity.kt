8e899e4c8c31a68c3e4e05ddb8ffddad28c94fc6
danielcout <daniel.couto01@etec.sp.gov.br>
2023-04-27 08:43:31 -0300

    Dice Roller 3

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -13,26 +13,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { rollDice() }
+        val rollButton: Button = findViewById(R.id.btnRoll)
+        rollButton.setOnClickListener {
+            //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            //toast.show()
+
+            rollDice()
+        }
 
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
 
-        val randomInt = (1..6).random()
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
         }
-        diceImage.setImageResource(drawableResource)
 
+        val sortRoll = dice.roll()
+        val sortTextView: TextView = findViewById(R.id.luckyNumber)
+        val txtResultado: TextView = findViewById(R.id.txtResultado)
+
+        sortTextView.text = sortRoll.toString();
+
+        if( diceRoll == sortRoll){
+            txtResultado.setText("Você Ganhou!")
+        }else{
+            txtResultado.setText("Você Perdeu!")
+        }
     }
 }
